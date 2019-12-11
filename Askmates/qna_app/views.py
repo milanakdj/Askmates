c9f3ed9f809450b5343ba310e4f88a5cc4c2b533
Milan Shrestha <you@example.com>
2019-12-11 16:08:01 +0545

    at the end of the day 4

--- Askmates/qna_app/views.py
+++ Askmates/qna_app/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
+from .models import QuestionModel,AnswerModel
 # Create your views here.
+
+
+def questions(request):
+    Question=QuestionModel.objects.all()
+    return render(request,'question.html',{'question':Question})
